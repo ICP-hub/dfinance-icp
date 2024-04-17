ac2c841bce5b23bb9c3c50ce935e187283359c58
hustlepreet-who-codes <harshpreet.singh@quadbtech.com>
2024-04-17 12:57:54 +0530

    Initial commit.

---
+++ src/icrc1_ledger_canister_frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { icrc1_ledger_canister_backend } from 'declarations/icrc1_ledger_canister_backend';
+
+function App() {
+  const [greeting, setGreeting] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    icrc1_ledger_canister_backend.greet(name).then((greeting) => {
+      setGreeting(greeting);
+    });
+    return false;
+  }
+
+  return (
+    <main>
+      <img src="/logo2.svg" alt="DFINITY logo" />
+      <br />
+      <br />
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" />
+        <button type="submit">Click Me!</button>
+      </form>
+      <section id="greeting">{greeting}</section>
+    </main>
+  );
+}
+
+export default App;

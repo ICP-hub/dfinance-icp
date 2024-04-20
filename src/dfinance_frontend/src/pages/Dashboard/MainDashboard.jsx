3df0c7d4df1db4fed10771a60244246edad34723
surajaswal-29 <suraj.aswal@quadbtech.com>
2024-04-20 22:33:30 +0530

    new updates

--- src/dfinance_frontend/src/pages/Dashboard/MainDashboard.jsx
+++ src/dfinance_frontend/src/pages/Dashboard/MainDashboard.jsx
@@ -12,7 +12,7 @@ const MainDashboard = ({children}) => {
 
   return (
     <>
-      <div className="w-full h-full xl3:w-[80%] xl4:w-[50%] xl3:mx-auto px-4 md:px-12 xl:px-24 relative">
+      <div className="w-full h-full xl3:w-[80%] xl4:w-[60%] xl3:mx-auto px-4 md:px-12 xl:px-24 relative">
         <div className="absolute top-0 right-0 xl:w-auto xl:h-auto -z-10">
           <Ellipse
             position={"top-right"}
@@ -24,7 +24,7 @@ const MainDashboard = ({children}) => {
         <Navbar isHomeNav={false} />
 
         {/* main area */}
-        <div className="w-full xl3:h-screen flex flex-col justify-center">
+        <div className="w-full">
           <DashboardNav />
           {children}
         </div>

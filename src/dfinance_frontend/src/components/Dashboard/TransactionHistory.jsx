800c413ea86467c785e1b8cc4a66e59000f2b006
vaibhavmali-git-quadb <vaibhav.mali@quadbtech.com>
2024-07-13 11:37:07 +0530

    added element in transacation history

--- src/dfinance_frontend/src/components/Dashboard/TransactionHistory.jsx
+++ src/dfinance_frontend/src/components/Dashboard/TransactionHistory.jsx
@@ -8,6 +8,7 @@ import 'react-toastify/dist/ReactToastify.css'; // Import toast styles
 import { useAuth } from "../../utils/useAuthClient"
 import { Modal } from "@mui/material"
 import { useDispatch, useSelector } from 'react-redux'
+import Element from "../../../public/Elements.svg"
 import {
   setIsWalletConnected,
   setWalletModalOpen
@@ -25,41 +26,41 @@ const TransactionHistory = () => {
   const navigate = useNavigate();
   const shouldRenderTransactionHistory =
     location.pathname === "/dashboard/transaction-history";
-    const dispatch = useDispatch()
-    const { isWalletCreated, isWalletModalOpen } = useSelector(state => state.utility)
-    const handleWalletConnect = () => {
-      console.log("connrcterd");
-      dispatch(setWalletModalOpen(!isWalletModalOpen))
-      // dispatch(setIsWalletCreated(true))
-    }
-  
-    const handleWallet = () => {
-      dispatch(setWalletModalOpen(!isWalletModalOpen))
-      dispatch(setIsWalletConnected(true))
+  const dispatch = useDispatch()
+  const { isWalletCreated, isWalletModalOpen } = useSelector(state => state.utility)
+  const handleWalletConnect = () => {
+    console.log("connrcterd");
+    dispatch(setWalletModalOpen(!isWalletModalOpen))
+    // dispatch(setIsWalletCreated(true))
+  }
+
+  const handleWallet = () => {
+    dispatch(setWalletModalOpen(!isWalletModalOpen))
+    dispatch(setIsWalletConnected(true))
+    navigate('/dashboard')
+  }
+  useEffect(() => {
+    if (isWalletCreated) {
       navigate('/dashboard')
     }
-    useEffect(() => {
-      if (isWalletCreated) {
-        navigate('/dashboard')
-      }
-    }, [isWalletCreated]);
-    useEffect(() => {
-      if (!isAuthenticated) {
-        navigate('/dashboard'); // Navigate to dashboard when wallet is disconnected
-      }
-    }, [isAuthenticated, history]);
-  
-    const [inputValue, setInputValue] = useState('');
-    const handleInputChange = (event) => {
-      setInputValue(event.target.value);
-    };
-  
-    const loginHandler = async (val) => {
-      await login(val);
-      // navigate("/");
-  
-      // await existingUserHandler();
-    };
+  }, [isWalletCreated]);
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate('/dashboard'); // Navigate to dashboard when wallet is disconnected
+    }
+  }, [isAuthenticated, history]);
+
+  const [inputValue, setInputValue] = useState('');
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const loginHandler = async (val) => {
+    await login(val);
+    // navigate("/");
+
+    // await existingUserHandler();
+  };
   // Function to handle copy action
   const copyToClipboard = (text) => {
     navigator.clipboard.writeText(text);
@@ -73,18 +74,26 @@ const TransactionHistory = () => {
       progress: undefined,
     });
   };
-  
+
   return (
-    <div className="w-full lg:w-12/12 mt-12">
+    <div className="relative w-full lg:w-12/12 mt-12">
+      {transactionHistory.length === 0 && <div className="absolute right-0 top-0 h-full md:w-1/2 pointer-events-none sxs3:w-[65%] z-[-1]">
+        <img
+          src={Element}
+          alt="Elements"
+          className="h-full w-full object-cover rounded-r-3xl opacity-70 filter drop-shadow-[0_0_0_#fffff] dark:opacity-40 dark:filter dark:drop-shadow-[0_0_0_#0000ff]"
+        />
+      </div>}
       {shouldRenderTransactionHistory && (
         <div className="w-full min-h-[500px] p-6 bg-gradient-to-r from-[#4659CF]/40 via-[#D379AB]/40 to-[#FCBD78]/40 rounded-3xl relative dark:bg-gradient dark:from-darkGradientStart dark:to-darkGradientEnd">
+
           <h1 className={`text-[#2A1F9D] font-bold text-xl md:text-2xl my-2  pb-4 dark:text-darkText ${transactionHistory.length > 0 ? 'lg:border-b-2 border-[#fff]' : ''}`}>
             Transaction History
           </h1>
           {transactionHistory.length === 0 ? (
             <div className="mt-[120px] flex flex-col justify-center align-center place-items-center">
               <div className="w-20 h-15">
-              <img src="/empty file.gif" alt="empty" className="w-30"/>
+                <img src="/empty file.gif" alt="empty" className="w-30" />
               </div>
               <p className="text-[#233D63] text-sm font-semibold dark:text-darkText">
                 Transaction History is not currently available for this market.
@@ -111,8 +120,8 @@ const TransactionHistory = () => {
                       <tr key={tx.id} className="w-full font-semibold text-[#4659CF]  hover:bg-[#ddf5ff8f] dark:hover:bg-[#5d59b0] rounded-lg">
                         <td className="py-2 px-4">
                           <div className="flex items-center dark:text-darkTextSecondary1">
-                          <span >{`${tx.hash.slice(0, 10)}...`}</span>
-          
+                            <span >{`${tx.hash.slice(0, 10)}...`}</span>
+
                             <button
                               className="ml-2 focus:outline-none"
                               onClick={() => copyToClipboard(tx.hash)}
@@ -228,59 +237,59 @@ const TransactionHistory = () => {
           )}
         </div>
       )}
-         {!isAuthenticated && <Modal open={isWalletModalOpen} onClose={handleWalletConnect}>
-            <div className='w-[300px] absolute bg-gray-100  shadow-xl rounded-lg top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 text-white dark:bg-darkOverlayBackground font-poppins'>
-              <h1 className='font-bold text-[#2A1F9D] dark:text-darkText'>Connect a wallet</h1>
-              <div className='flex flex-col gap-2 mt-3 text-sm'>
-                <div className="w-full flex items-center justify-between bg-[#c8c8c8] bg-opacity-20 hover:bg-[#b7b4b4] cursor-pointer p-2 rounded-md text-[#2A1F9D] dark:bg-darkBackground/30 dark:hover:bg-[#8782d8] dark:text-darkText" onClick={() => loginHandler("ii")}>
-                  Internet Identity
-                  <div className='w-8 h-8'>
-                    <img src={"https://i.pinimg.com/originals/12/33/64/123364eb4e844960c2fd6ebffccba0a0.png"} alt="connect_wallet_icon" className='object-fill w-8 h-8' />
-                  </div>
-                </div>
-                <div className="w-full flex items-center justify-between bg-[#c8c8c8] bg-opacity-20 hover:bg-[#b7b4b4] cursor-pointer p-2 rounded-md text-[#2A1F9D] dark:bg-darkBackground/30 dark:hover:bg-[#b7b4b4] dark:text-darkText">
-                  Plug
-                  <div className='w-8 h-8'>
-                    <img src={"/plug.png.png"} alt="connect_wallet_icon" className='object-fill w-8 h-8' />
-                  </div>
-                </div>
-                <div className="w-full flex items-center justify-between bg-[#c8c8c8] bg-opacity-20 hover:bg-[#b7b4b4] cursor-pointer p-2 rounded-md text-[#2A1F9D] dark:bg-darkBackground/30 dark:hover:bg-[#b7b4b4] dark:text-darkText">
-                  Bifinity
-                  <div className='w-8 h-8'>
-                    <img src={"/bifinity.png"} alt="connect_wallet_icon" className='object-fill w-8 h-8' />
-                  </div>
-                </div>
-                <div className="w-full flex items-center justify-between bg-[#c8c8c8] bg-opacity-20 hover:bg-[#b7b4b4] cursor-pointer p-2 rounded-md text-[#2A1F9D] dark:bg-darkBackground/30 dark:hover:bg-[#b7b4b4] dark:text-darkText" onClick={() => loginHandler("nfid")}>
-                  NFID
-                  <div className='w-8 h-8'>
-                    <img src={"/nfid.png"} alt="connect_wallet_icon" className='object-fill w-8 h-8' />
-                  </div>
-                </div>
+      {!isAuthenticated && <Modal open={isWalletModalOpen} onClose={handleWalletConnect}>
+        <div className='w-[300px] absolute bg-gray-100  shadow-xl rounded-lg top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 text-white dark:bg-darkOverlayBackground font-poppins'>
+          <h1 className='font-bold text-[#2A1F9D] dark:text-darkText'>Connect a wallet</h1>
+          <div className='flex flex-col gap-2 mt-3 text-sm'>
+            <div className="w-full flex items-center justify-between bg-[#c8c8c8] bg-opacity-20 hover:bg-[#b7b4b4] cursor-pointer p-2 rounded-md text-[#2A1F9D] dark:bg-darkBackground/30 dark:hover:bg-[#8782d8] dark:text-darkText" onClick={() => loginHandler("ii")}>
+              Internet Identity
+              <div className='w-8 h-8'>
+                <img src={"https://i.pinimg.com/originals/12/33/64/123364eb4e844960c2fd6ebffccba0a0.png"} alt="connect_wallet_icon" className='object-fill w-8 h-8' />
               </div>
-              <p className='w-full  text-xs my-3 text-gray-600 dark:text-[#CDB5AC]'>Track wallet balance in read-only mode</p>
-
-              <div className="w-full">
-                <input
-                  type="text"
-                  value={inputValue}
-                  onChange={handleInputChange}
-                  className="w-full p-2 border border-[#233D63] focus:outline-none focus:border-blue-500 placeholder:text-[#233D63] dark:border-darkTextSecondary1 dark:placeholder:text-darkTextSecondary1 text-gray-600 dark:text-darkTextSecondary1 text-xs rounded-md dark:bg-transparent"
-                  placeholder="Enter ethereum address or username"
-                />
-              </div>
-
-              {inputValue && (
-                <div className="w-full flex mt-3">
-                  <Button
-                    title="Connect"
-                    onClickHandler={handleWallet}
-                    className="w-full my-2 bg-gradient-to-r text-white from-[#EB8863] to-[#81198E] rounded-md p-3 px-20 shadow-lg font-semibold text-sm"
-                  />
-                </div>
-              )}
-
             </div>
-          </Modal>}
+            <div className="w-full flex items-center justify-between bg-[#c8c8c8] bg-opacity-20 hover:bg-[#b7b4b4] cursor-pointer p-2 rounded-md text-[#2A1F9D] dark:bg-darkBackground/30 dark:hover:bg-[#b7b4b4] dark:text-darkText">
+              Plug
+              <div className='w-8 h-8'>
+                <img src={"/plug.png.png"} alt="connect_wallet_icon" className='object-fill w-8 h-8' />
+              </div>
+            </div>
+            <div className="w-full flex items-center justify-between bg-[#c8c8c8] bg-opacity-20 hover:bg-[#b7b4b4] cursor-pointer p-2 rounded-md text-[#2A1F9D] dark:bg-darkBackground/30 dark:hover:bg-[#b7b4b4] dark:text-darkText">
+              Bifinity
+              <div className='w-8 h-8'>
+                <img src={"/bifinity.png"} alt="connect_wallet_icon" className='object-fill w-8 h-8' />
+              </div>
+            </div>
+            <div className="w-full flex items-center justify-between bg-[#c8c8c8] bg-opacity-20 hover:bg-[#b7b4b4] cursor-pointer p-2 rounded-md text-[#2A1F9D] dark:bg-darkBackground/30 dark:hover:bg-[#b7b4b4] dark:text-darkText" onClick={() => loginHandler("nfid")}>
+              NFID
+              <div className='w-8 h-8'>
+                <img src={"/nfid.png"} alt="connect_wallet_icon" className='object-fill w-8 h-8' />
+              </div>
+            </div>
+          </div>
+          <p className='w-full  text-xs my-3 text-gray-600 dark:text-[#CDB5AC]'>Track wallet balance in read-only mode</p>
+
+          <div className="w-full">
+            <input
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+              className="w-full p-2 border border-[#233D63] focus:outline-none focus:border-blue-500 placeholder:text-[#233D63] dark:border-darkTextSecondary1 dark:placeholder:text-darkTextSecondary1 text-gray-600 dark:text-darkTextSecondary1 text-xs rounded-md dark:bg-transparent"
+              placeholder="Enter ethereum address or username"
+            />
+          </div>
+
+          {inputValue && (
+            <div className="w-full flex mt-3">
+              <Button
+                title="Connect"
+                onClickHandler={handleWallet}
+                className="w-full my-2 bg-gradient-to-r text-white from-[#EB8863] to-[#81198E] rounded-md p-3 px-20 shadow-lg font-semibold text-sm"
+              />
+            </div>
+          )}
+
+        </div>
+      </Modal>}
     </div>
   );
 };

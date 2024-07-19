1f87e646a52152f3d6936a325f7083c849ccf38c
Shreya Mahajan <shreya.mahajan@quadbtech.com>
2024-07-19 18:11:46 +0530

    fixes

--- src/dfinance_frontend/src/components/Dashboard/PaymentDone.jsx
+++ src/dfinance_frontend/src/components/Dashboard/PaymentDone.jsx
@@ -2,23 +2,23 @@ import { Check, Wallet , X  } from 'lucide-react';
 
 import React, { useState } from 'react';
 
-const PaymentDone = ({ asset }) => {
-    const [isVisible, setIsVisible] = useState(true);
+const PaymentDone = ({ asset, isModalOpen, handleModalOpen, }) => {
+//     const [isVisible, setIsVisible] = useState(true);
+// console.log("my model is open or close ", isVisible)
+//     const handleClose = () => {
+//         setIsVisible(false);
+//     };
 
-    const handleClose = () => {
-        setIsVisible(false);
-    };
-
-    if (!isVisible) {
-        return null;
+//     if (!isVisible) {
+//         return null;
         
-         // Hide the component when isVisible is false
-    }
+//          // Hide the component when isVisible is false
+//     }
 
     return (
-        <div className="w-[400px] h-[380px] absolute bg-white shadow-xl filter backdrop-blur-lg rounded-lg top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 text-[#2A1F9D] dark:bg-[#252347] dark:text-darkText z-50">
+        // <div className="w-[400px] h-[380px] absolute bg-white shadow-xl filter backdrop-blur-lg rounded-lg top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 text-[#2A1F9D] dark:bg-[#252347] dark:text-darkText z-50">
             <div className="w-full flex flex-col items-center">
-                <button onClick={handleClose} className="text-gray-400 hover:text-gray-600 focus:outline-none self-end">
+                <button open={isModalOpen} onClose={handleModalOpen} className="text-gray-400 hover:text-gray-600 focus:outline-none self-end">
                     <X size={24} />
                 </button>
                 <div className='border rounded-full p-2 my-3 text-green-500 border-green-500'>
@@ -37,13 +37,13 @@ const PaymentDone = ({ asset }) => {
                     </button>
                 </div>
                 <button 
-                    onClick={handleClose}
+                   open={isModalOpen} onClick={handleModalOpen}
                     className="bg-gradient-to-tr from-[#ffaf5a] to-[#81198E] w-full text-white rounded-md p-2 px-4 shadow-md font-semibold text-sm mt-4"
                 >
                     Close Now
                 </button>
             </div>
-        </div>
+        // </div>
     );
 };
 

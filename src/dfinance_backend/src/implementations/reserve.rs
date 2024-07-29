a4e1f04bece49407229f6ddb856ffad39414083d
somyarsahu <somya.sahu@quadbtech.com>
2024-07-30 01:29:37 +0530

    Current changes

--- src/dfinance_backend/src/implementations/reserve.rs
+++ src/dfinance_backend/src/implementations/reserve.rs
@@ -1,32 +1,32 @@
-// use crate::{
-//     api::state_handler::mutate_state,
-//     declarations::{assets::ReserveData, storable::Candid},
-// };
+use crate::{
+    api::state_handler::mutate_state,
+    declarations::{assets::ReserveData, storable::Candid},
+};
 
-// use candid::Principal;
-// pub fn initialize_reserve() {
-//     let data = ReserveData {
-//         last_update_timestamp: Some(ic_cdk::api::time()),
-//         current_liquidity_rate: Some(10000000),
-//         current_stable_borrow_rate: Some(8),
-//         current_variable_borrow_rate: Some(4),
-//         interest_rate_strategy_address: None,
-//         d_token_canister: Some(Principal::from_text("hbrpn-74aaa-aaaaa-qaaxq-cai").unwrap()),
-//         stable_debt_token_canister: Some(
-//             Principal::from_text("fsomn-xeaaa-aaaaa-qaaza-cai").unwrap(),
-//         ),
-//         variable_debt_token_canister: Some(
-//             Principal::from_text("fsomn-xeaaa-aaaaa-qaaza-cai").unwrap(),
-//         ),
-//         liquidity_index: Some(10000000000),
-//         id: 1,
-//         variable_borrow_index: Some(5000000000),
-//     };
+use candid::Principal;
+pub fn initialize_reserve() {
+    let data = ReserveData {
+        last_update_timestamp: Some(ic_cdk::api::time()),
+        current_liquidity_rate: Some(10000000),
+        current_stable_borrow_rate: Some(8),
+        current_variable_borrow_rate: Some(4),
+        interest_rate_strategy_address: None,
+        d_token_canister: Some(Principal::from_text("hbrpn-74aaa-aaaaa-qaaxq-cai").unwrap()),
+        stable_debt_token_canister: Some(
+            Principal::from_text("fsomn-xeaaa-aaaaa-qaaza-cai").unwrap(),
+        ),
+        variable_debt_token_canister: Some(
+            Principal::from_text("fsomn-xeaaa-aaaaa-qaaza-cai").unwrap(),
+        ),
+        liquidity_index: Some(10000000000),
+        id: 1,
+        variable_borrow_index: Some(5000000000),
+    };
 
-//     mutate_state(|state| {
-//         let reserve_data = &mut state.asset_index;
-//         reserve_data.insert("ckbtc".to_string(), Candid(data));
-//         ic_cdk::println!("Reserve initialized successfully");
-//     });
-// }
+    mutate_state(|state| {
+        let reserve_data = &mut state.asset_index;
+        reserve_data.insert("ckbtc".to_string(), Candid(data));
+        ic_cdk::println!("Reserve initialized successfully");
+    });
+}
 
--- src/dfinance_backend/src/protocol/libraries/logic/borrow.rs
+++ src/dfinance_backend/src/protocol/libraries/logic/borrow.rs
@@ -1,216 +1,217 @@
-// use candid::{Nat, Principal};
-// use ic_cdk::api::call::{call, CallResult};
-// use ic_cdk::caller;
-// use std::env;
+use candid::{Nat, Principal};
+use ic_cdk::api::call::{call, CallResult};
+use ic_cdk::caller;
+use std::env;
 
-// use crate::api::deposit::asset_transfer_from;
-// use crate::api::state_handler::*;
-// use crate::declarations::assets::ExecuteBorrowParams;
-// use crate::protocol::libraries::logic::{reserve::ReserveLogic, validation::ValidationLogic};
-// use crate::declarations::assets::InterestRateMode;
-//use crate::declarations::assets::ExecuteRepayParams;
-// pub async fn execute_borrow(params: ExecuteBorrowParams) {
-// Fetches the canister ids, user principal and amount from env
-//     let canister_id_ckbtc_ledger = env::var("CANISTER_ID_CKBTC_LEDGER")
-//         .expect("CANISTER_ID_CKBTC_LEDGER environment variable not set");
-//     let canister_id_dfinance_backend = env::var("CANISTER_ID_DFINANCE_BACKEND")
-//         .expect("CANISTER_ID_CKBTC_LEDGER environment variable not set");
-//     let dtoken_canister_id =
-//         env::var("CANISTER_ID_DTOKEN").expect("CANISTER_ID_DTOKEN environment variable not set");
+use crate::api::deposit::asset_transfer_from;
+use crate::api::state_handler::*;
+use crate::declarations::assets::ExecuteBorrowParams;
+use crate::protocol::libraries::logic::{reserve::ReserveLogic, validation::ValidationLogic};
+use crate::declarations::assets::InterestRateMode;
+use crate::declarations::assets::ExecuteRepayParams;
 
-//     let debttoken_canister_id =
-//         env::var("CANISTER_ID_DEBTTOKEN").expect("CANISTER_ID_DEBTTOKEN environment variable not set");
+pub async fn execute_borrow(params: ExecuteBorrowParams) {
+// // Fetches the canister ids, user principal and amount from env
+    let canister_id_ckbtc_ledger = env::var("CANISTER_ID_CKBTC_LEDGER")
+        .expect("CANISTER_ID_CKBTC_LEDGER environment variable not set");
+    let canister_id_dfinance_backend = env::var("CANISTER_ID_DFINANCE_BACKEND")
+        .expect("CANISTER_ID_CKBTC_LEDGER environment variable not set");
+    let dtoken_canister_id =
+        env::var("CANISTER_ID_DTOKEN").expect("CANISTER_ID_DTOKEN environment variable not set");
 
-//     let ledger_canister_id =
-//         Principal::from_text(canister_id_ckbtc_ledger).expect("Invalid ledger canister ID");
-// let debttoken_ledger_canister_id =
-//         Principal::from_text(dtoken_canister_id).expect("Invalid ledger canister ID");
-//     let user_principal = caller();
-//     let platform_principal =
-//         Principal::from_text(canister_id_dfinance_backend).expect("Invalid platform principal");
-//     let amount_nat = Nat::from(params.amount);
+    let debttoken_canister_id =
+        env::var("CANISTER_ID_DEBTTOKEN").expect("CANISTER_ID_DEBTTOKEN environment variable not set");
 
-//     // Reads the reserve_data from the ASSET_INDEX using the asset key
-//     let reserve_data = mutate_state(|state| {
-//         let asset_index = &mut state.asset_index;
-//         asset_index
-//             .get(&params.asset)
-//             .expect("Reserve not found")
-//             .clone()
-//     });
+    let ledger_canister_id =
+        Principal::from_text(canister_id_ckbtc_ledger).expect("Invalid ledger canister ID");
+let debttoken_ledger_canister_id =
+        Principal::from_text(dtoken_canister_id).expect("Invalid ledger canister ID");
+    let user_principal = caller();
+    let platform_principal =
+        Principal::from_text(canister_id_dfinance_backend).expect("Invalid platform principal");
+    let amount_nat = Nat::from(params.amount);
 
-//     // Fetches the reserve logic cache having the current values
-//     let reserve_cache = ReserveLogic::cache(reserve_data.clone());
+    // Reads the reserve_data from the ASSET_INDEX using the asset key
+    let reserve_data = mutate_state(|state| {
+        let asset_index = &mut state.asset_index;
+        asset_index
+            .get(&params.asset)
+            .expect("Reserve not found")
+            .clone()
+    });
 
-//     // Updates the liquidity and borrow index
-//     ReserveLogic::update_state(&reserve_data, &reserve_cache);
+    // Fetches the reserve logic cache having the current values
+    let reserve_cache = ReserveLogic::cache(reserve_data.clone());
 
-//     // Validates supply using the reserve_data
-//     ValidationLogic::validate_borrow(&reserve_cache, &reserve_data, params.amount);
+    // Updates the liquidity and borrow index
+    ReserveLogic::update_state(&reserve_data, &reserve_cache);
 
-// let currentStableRate : Nat = 0;
-// let isFirstBorrowing = false;
+    // Validates supply using the reserve_data
+    ValidationLogic::validate_borrow(&reserve_cache, &reserve_data, params.amount);
 
-// if (params.interestRateMode == InterestRateMode::Stable) {
-//     currentStableRate = reserve.currentStableBorrowRate;
+let currentStableRate : Nat = 0;
+let isFirstBorrowing = false;
 
-//mints the debt token
-// asset_transfer_from(
-//         debttoken_ledger_canister_id,
-//         platform_principal,
-//         user_principal,
-//         amount_nat.clone(),
-//     )
-//     .await;
-//   } else {
-// asset_transfer_from(
-//     debttoken_ledger_canister_id,
-//     platform_principal,
-//     user_principal,
-//     amount_nat.clone(),
-// )
-// .await;
+if (params.interestRateMode == InterestRateMode::Stable) {
+    currentStableRate = reserve.currentStableBorrowRate;
 
-//   if (isFirstBorrowing) {
-//     userConfig.setBorrowing(reserve.id, true);
-//   }
+// // mints the debt token
+asset_transfer_from(
+        debttoken_ledger_canister_id,
+        platform_principal,
+        user_principal,
+        amount_nat.clone(),
+    )
+    .await;
+  } else {
+asset_transfer_from(
+    debttoken_ledger_canister_id,
+    platform_principal,
+    user_principal,
+    amount_nat.clone(),
+)
+.await;
 
-//     // Updates inetrest rates with the assets and the amount
-//     ReserveLogic::update_interest_rates(
-//         &reserve_data,
-//         &reserve_cache,
-//         params.asset,
-//         params.amount,
-//         0,
-//     );
+  if (isFirstBorrowing) {
+    userConfig.setBorrowing(reserve.id, true);
+  }
 
-//     // Transfers the asset from the user to our backend cansiter
-//if (params.release_underlying){
+    // Updates inetrest rates with the assets and the amount
+    ReserveLogic::update_interest_rates(
+        &reserve_data,
+        &reserve_cache,
+        params.asset,
+        params.amount,
+        0,
+    );
 
-//     asset_transfer_from(
-//         ledger_canister_id,
-//         user_principal,
-//         platform_principal,
-//         amount_nat.clone(),
-//     )
-//     .await;
-//}
-//     // Inter canister call to execute dtoken transfer
+    // Transfers the asset from the user to our backend cansiter
+if (params.release_underlying){
 
-//     // --------- Isolation mode logic (TODO) ---------
-//     // If first_supply == true : Validate automatic use as collateral
-//     // If first_supply == false : Set using as collateral
-// }
+    asset_transfer_from(
+        ledger_canister_id,
+        user_principal,
+        platform_principal,
+        amount_nat.clone(),
+    )
+    .await;
+}
+    // Inter canister call to execute dtoken transfer
 
-// fn  executeRepay(params: ExecuteRepayParams)  {
+    // --------- Isolation mode logic (TODO) ---------
+    // If first_supply == true : Validate automatic use as collateral
+    // If first_supply == false : Set using as collateral
+}
 
-// Fetches the canister ids, user principal and amount from env
-//     let canister_id_ckbtc_ledger = env::var("CANISTER_ID_CKBTC_LEDGER")
-//         .expect("CANISTER_ID_CKBTC_LEDGER environment variable not set");
-//     let canister_id_dfinance_backend = env::var("CANISTER_ID_DFINANCE_BACKEND")
-//         .expect("CANISTER_ID_CKBTC_LEDGER environment variable not set");
-//     let dtoken_canister_id =
-//         env::var("CANISTER_ID_DTOKEN").expect("CANISTER_ID_DTOKEN environment variable not set");
+fn  executeRepay(params: ExecuteRepayParams)  {
 
-//     let debttoken_canister_id =
-//         env::var("CANISTER_ID_DEBTTOKEN").expect("CANISTER_ID_DEBTTOKEN environment variable not set");
+// // Fetches the canister ids, user principal and amount from env
+    let canister_id_ckbtc_ledger = env::var("CANISTER_ID_CKBTC_LEDGER")
+        .expect("CANISTER_ID_CKBTC_LEDGER environment variable not set");
+    let canister_id_dfinance_backend = env::var("CANISTER_ID_DFINANCE_BACKEND")
+        .expect("CANISTER_ID_CKBTC_LEDGER environment variable not set");
+    let dtoken_canister_id =
+        env::var("CANISTER_ID_DTOKEN").expect("CANISTER_ID_DTOKEN environment variable not set");
 
-//     let ledger_canister_id =
-//         Principal::from_text(canister_id_ckbtc_ledger).expect("Invalid ledger canister ID");
-// let debttoken_ledger_canister_id =
-//         Principal::from_text(dtoken_canister_id).expect("Invalid ledger canister ID");
-//     let user_principal = caller();
-//     let platform_principal =
-//         Principal::from_text(canister_id_dfinance_backend).expect("Invalid platform principal");
-// let dtoken_ledger_canister_id =
-//         Principal::from_text(dtoken_canister_id).expect("Invalid ledger canister ID");
-//     let amount_nat = Nat::from(params.amount);
+    let debttoken_canister_id =
+        env::var("CANISTER_ID_DEBTTOKEN").expect("CANISTER_ID_DEBTTOKEN environment variable not set");
 
-//     let reserve_data = mutate_state(|state| {
-//         let asset_index = &mut state.asset_index;
-//         asset_index
-//             .get(&params.asset)
-//             .expect("Reserve not found")
-//             .clone()
-//     });
+    let ledger_canister_id =
+        Principal::from_text(canister_id_ckbtc_ledger).expect("Invalid ledger canister ID");
+let debttoken_ledger_canister_id =
+        Principal::from_text(dtoken_canister_id).expect("Invalid ledger canister ID");
+    let user_principal = caller();
+    let platform_principal =
+        Principal::from_text(canister_id_dfinance_backend).expect("Invalid platform principal");
+let dtoken_ledger_canister_id =
+        Principal::from_text(dtoken_canister_id).expect("Invalid ledger canister ID");
+    let amount_nat = Nat::from(params.amount);
 
-//     // Fetches the reserve logic cache having the current values
-//     let reserve_cache = ReserveLogic::cache(reserve_data.clone());
+    let reserve_data = mutate_state(|state| {
+        let asset_index = &mut state.asset_index;
+        asset_index
+            .get(&params.asset)
+            .expect("Reserve not found")
+            .clone()
+    });
 
-//     // Updates the liquidity and borrow index
-//     ReserveLogic::update_state(&reserve_data, &reserve_cache);
+    // Fetches the reserve logic cache having the current values
+    let reserve_cache = ReserveLogic::cache(reserve_data.clone());
 
-//     // Validates supply using the reserve_data
-//     ValidationLogic::validate_repay(&reserve_cache, &reserve_data, params.amount);
+    // Updates the liquidity and borrow index
+    ReserveLogic::update_state(&reserve_data, &reserve_cache);
 
-// if let paybackAmount :Nat = params.interestRateMode == InterestRateMode::Stable{
-//     reserve.currentStableDebt;
-// }else{
-//     reserve.currentVariableDebt;
+    // Validates supply using the reserve_data
+    ValidationLogic::validate_repay(&reserve_cache, &reserve_data, params.amount);
 
-// }
-// let args = Account {
-//     owner: ic_cdk::caller(),
-//     subaccount: None, // Or Some(subaccount_bytes) if you have a subaccount
-// };
+if let paybackAmount :Nat = params.interestRateMode == InterestRateMode::Stable{
+    reserve.currentStableDebt;
+}else{
+    reserve.currentVariableDebt;
 
-// let (result,): (TransferFromResult,) = call(dtoken, "icrc1_balanceOf", (args,))
-//     .await
-//     .map_err(|e| e.1)?;
+}
+let args = Account {
+    owner: ic_cdk::caller(),
+    subaccount: None, // Or Some(subaccount_bytes) if you have a subaccount
+};
 
-// params.amount = result.amount;
+let (result,): (TransferFromResult,) = call(dtoken, "icrc1_balanceOf", (args,))
+    .await
+    .map_err(|e| e.1)?;
 
-//   if (params.amount < paybackAmount) {
-//     paybackAmount = params.amount;
-//   }
+params.amount = result.amount;
 
-//burn the tokens
+  if (params.amount < paybackAmount) {
+    paybackAmount = params.amount;
+  }
 
-// if (params.interestRateMode == InterestRateMode::Stable) {
-//     currentStableRate = reserve.currentStableBorrowRate;
+// burn the tokens
 
-// asset_transfer_from(
-//         debttoken_ledger_canister_id,
-//         user_principal,
-//         platform_principal,
-//
-//         amount_nat.clone(),
-//     )
-//     .await;
-//   } else {
-// asset_transfer_from(
-//     debttoken_ledger_canister_id,
-//     user_principal,
-//     platform_principal,
-//     amount_nat.clone(),
-// )
-// .await;
+if (params.interestRateMode == InterestRateMode::Stable) {
+    currentStableRate = reserve.currentStableBorrowRate;
 
-//burn the token
-//if(params.use_dtokens){
+asset_transfer_from(
+        debttoken_ledger_canister_id,
+        user_principal,
+        platform_principal,
 
-// asset_transfer_from(
-//     dtoken_ledger_canister_id,
-//     user_principal,
-//     platform_principal,
-//     amount_nat.clone(),
-// )
-// .await;
+        amount_nat.clone(),
+    )
+    .await;
+  } else {
+asset_transfer_from(
+    debttoken_ledger_canister_id,
+    user_principal,
+    platform_principal,
+    amount_nat.clone(),
+)
+.await;
 
-// }else{
+// burn the token
+if(params.use_dtokens){
 
-//     asset_transfer_from(
-//         ledger_canister_id,
-//         user_principal,
-//         platform_principal,
-//         amount_nat.clone(),
-//     )
-//     .await;
-//}
+asset_transfer_from(
+    dtoken_ledger_canister_id,
+    user_principal,
+    platform_principal,
+    amount_nat.clone(),
+)
+.await;
 
-// }
+}else{
 
-//   }
+    asset_transfer_from(
+        ledger_canister_id,
+        user_principal,
+        platform_principal,
+        amount_nat.clone(),
+    )
+    .await;
+}
+
+}
+
+  }
 
 use crate::api::state_handler::read_state;
 use crate::declarations::assets::ReserveData;

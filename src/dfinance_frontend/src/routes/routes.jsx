3960d3c753ff9e9c3eb3182def56accc17cb975f
somyarsahu <somya.sahu@quadbtech.com>
2024-08-13 12:12:13 +0530

    Removed gov route

--- src/dfinance_frontend/src/routes/routes.jsx
+++ src/dfinance_frontend/src/routes/routes.jsx
@@ -5,10 +5,10 @@ import Home from "../pages/Home/Home"
 
 import Error from "../pages/Error"
 import AssetDetails from "../components/Dashboard/AssetDetails"
-import DFinanceGov from "../pages/Governance/DFinanceGov"
-import ProposalDetails from "../components/Governance/ProposalDetails"
-import MySupply from "../components/Dashboard/MySupply"
-import { elements } from "chart.js"
+// import DFinanceGov from "../pages/Governance/DFinanceGov"
+// import ProposalDetails from "../components/Governance/ProposalDetails"
+// import MySupply from "../components/Dashboard/MySupply"
+// import { elements } from "chart.js"
 import TransactionHistoryBox from "../components/Dashboard/TransactionHistory"
 import StakeDetails from "../pages/Stake/Stake"
 import Faucet from "../pages/Faucet/faucet"
@@ -68,22 +68,22 @@ export default [
       </MainDashboard>
     ),
   },
-  {
-    path: "/governance",
-    element: (
-      <MainDashboard includeDashboardNav={false}>
-        <DFinanceGov  />
-      </MainDashboard>
-    ),
-  },
-  {
-    path: "/governance/proposal-details",
-    element: (
-      <MainDashboard>
-        <ProposalDetails />
-      </MainDashboard>
-    ),
-  },
+  // {
+  //   path: "/governance",
+  //   element: (
+  //     <MainDashboard includeDashboardNav={false}>
+  //       <DFinanceGov  />
+  //     </MainDashboard>
+  //   ),
+  // },
+  // {
+  //   path: "/governance/proposal-details",
+  //   element: (
+  //     <MainDashboard>
+  //       <ProposalDetails />
+  //     </MainDashboard>
+  //   ),
+  // },
   {
     path: "/stake",
     element: (

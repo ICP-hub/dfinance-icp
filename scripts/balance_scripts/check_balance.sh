fbed9368e65d4a2ed6449fc56c4f09fdffd79dce
anshika-Quadbcode <anshika.goel@quadbtech.com>
2024-09-19 10:56:53 +0530

    custom-token-ledger

--- scripts/balance_scripts/check_balance.sh
+++ scripts/balance_scripts/check_balance.sh
@@ -6,7 +6,7 @@ set -e
 check_balance() {
     local principal=$1
     local label=$2
-    local canister_id="cketh_ledger"
+    local canister_id="ckbtc_ledger"
     local balance_method="icrc1_balance_of"
 
     # Check the balance
@@ -18,17 +18,17 @@ check_balance() {
 
 # Get the principal for backend_canister
 backend_canister_principal=$(dfx canister id dfinance_backend)
-dtoken_canister=$(dfx canister id dtoken)
-user1="i33yh-4wqd6-4nept-op4lj-oqe6v-27zvz-o32nr-rnx64-wnht6-rzioe-lae"
+# dtoken_canister=$(dfx canister id dcketh)
+user3="ajuq4-ruaaa-aaaaa-qaaga-cai"
 # Check balance and principal for each identity
 echo "Checking balance and principal for identities and backend canister..."
 
 check_balance $(dfx identity get-principal --identity anonymous) "Anonymous"
 check_balance $(dfx identity get-principal --identity default) "Default"
-check_balance $(dfx identity get-principal --identity abcd) "abcd"
-check_balance $(dfx identity get-principal --identity user1) "User1"
+check_balance $(dfx identity get-principal --identity newminter) "newminter"
+check_balance $user3 "User1"
 check_balance $backend_canister_principal "Backend Canister"
-check_balance $dtoken_canister "DToken Canister"
+# check_balance $dtoken_canister "DToken Canister"
 # Switch back to the default identity at the end
 dfx identity use default
 echo "Switched back to default identity."
---
+++ scripts/deploy_scripts/deploy_token_ledger.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+source ../../.env
+
+# Set token name and symbol
+export TOKEN_NAME="dckETH"
+echo "Token Name: $TOKEN_NAME"
+
+export TOKEN_SYMBOL="dckETH"
+
+# Set default principal (used for minting account)
+export BACKEND=$CANISTER_ID_DFINANCE_BACKEND
+
+# Set pre-minted tokens and transfer fee (initial circulation is set to 0)
+export PRE_MINTED_TOKENS=0
+export TRANSFER_FEE=0
+
+export 
+
+# Set archive-related parameters
+export TRIGGER_THRESHOLD=2000
+export NUM_OF_BLOCK_TO_ARCHIVE=1000
+export CYCLE_FOR_ARCHIVE_CREATION=10000000000000
+
+# Set feature flags (optional)
+export FEATURE_FLAGS=false
+
+# Deploy the token canister with the specified arguments
+dfx deploy token_ledger --argument "(variant { Init = record {
+     token_symbol = \"${TOKEN_SYMBOL}\";
+     token_name = \"${TOKEN_NAME}\";
+     minting_account = record { owner = principal \"${BACKEND}\" };
+     transfer_fee = ${TRANSFER_FEE};
+     metadata = vec {};
+     feature_flags = opt record { icrc2 = ${FEATURE_FLAGS} };
+     initial_balances = vec {};  
+     archive_options = record {
+         num_blocks_to_archive = ${NUM_OF_BLOCK_TO_ARCHIVE};
+         trigger_threshold = ${TRIGGER_THRESHOLD};
+         controller_id = principal \"${BACKEND}\";
+         cycles_for_archive_creation = opt ${CYCLE_FOR_ARCHIVE_CREATION};
+     };
+ }})"
+
+echo "dckETH has been successfully deployed with initial circulation set to 0."

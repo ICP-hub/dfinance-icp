806b70ddc84f3ad6af34fd8354244d7e63cbd9c6
Shreya Mahajan <shreya.mahajan@quadbtech.com>
2024-10-07 21:15:24 +0530

    fix

--- src/dfinance_frontend/src/components/Dashboard/DashboardPopup/WithdrawPopup.jsx
+++ src/dfinance_frontend/src/components/Dashboard/DashboardPopup/WithdrawPopup.jsx
@@ -448,10 +448,11 @@ const WithdrawPopup = ({
               <div className="w-full flex justify-between items-center my-1">
                 <p>Supply APY</p>
                 <p>
-                  {supplyRateAPR * 100 < 0.1
-                    ? "<0.1%"
-                    : `${supplyRateAPR * 100}%`}
-                </p>
+  {supplyRateAPR * 100 < 0.1
+    ? "<0.1%"
+    : `${(supplyRateAPR * 100).toFixed(2)}%`}
+</p>
+
               </div>
               <div className="w-full flex justify-between items-center my-1">
                 <p>Collateralization</p>

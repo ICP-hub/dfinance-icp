f79232b7e2b96cdb60e18a4eb409d78819e91dda
jyotirmaygupta23 <jyotirmay.gupta@quadbtech.com>
2024-10-23 18:06:17 +0530

    validation

--- src/dfinance_backend/src/protocol/libraries/math/calculate.rs
+++ src/dfinance_backend/src/protocol/libraries/math/calculate.rs
@@ -3,7 +3,6 @@ use ic_xrc_types::{Asset, AssetClass, GetExchangeRateRequest, GetExchangeRateRes
 
 use super::math_utils::ScalingMath;
 
-
 #[derive(CandidType, Deserialize, Clone, Debug)]
 pub struct UserPosition {
     pub total_collateral_value: u128,
@@ -16,44 +15,47 @@ pub fn calculate_health_factor(position: &UserPosition) -> u128 {
     }
 
     (position.total_collateral_value * position.liquidation_threshold)
-        / position.total_borrowed_value                                   
+        / position.total_borrowed_value
 }
 
 pub fn calculate_ltv(position: &UserPosition) -> u128 {
     if position.total_collateral_value == 0 {
-        return 0; 
+        return 0;
     }
 
-    position.total_borrowed_value.scaled_div(position.total_collateral_value)
+    position
+        .total_borrowed_value
+        .scaled_div(position.total_collateral_value)
 }
 
 pub fn cal_average_threshold(
-    amount: u128, 
+    amount: u128,
     amount_taken: u128,
-    reserve_liq_thres: u128, 
-    user_total_collateral: u128, 
-    user_liq_thres: u128
+    reserve_liq_thres: u128,
+    user_total_collateral: u128,
+    user_liq_thres: u128,
 ) -> u128 {
-    
-    let result = ((amount.scaled_mul(reserve_liq_thres)) + (user_total_collateral.scaled_mul(user_liq_thres)) - (amount_taken.scaled_mul(reserve_liq_thres))).scaled_div(amount + user_total_collateral - amount_taken);
+    let result = ((amount.scaled_mul(reserve_liq_thres))
+        + (user_total_collateral.scaled_mul(user_liq_thres))
+        - (amount_taken.scaled_mul(reserve_liq_thres)))
+    .scaled_div(amount + user_total_collateral - amount_taken);
     result
 }
 
-
 pub fn cal_average_ltv(
-    amount: u128, 
+    amount: u128,
     amount_taken: u128,
-    reserve_ltv: u128, 
-    user_total_collateral: u128, 
-    user_max_ltv: u128
+    reserve_ltv: u128,
+    user_total_collateral: u128,
+    user_max_ltv: u128,
 ) -> u128 {
-    
-    let result = ((amount.scaled_mul(reserve_ltv)) + (user_total_collateral.scaled_mul(user_max_ltv)) - (amount_taken.scaled_mul(reserve_ltv))).scaled_div(amount + user_total_collateral - amount_taken);
+    let result = ((amount.scaled_mul(reserve_ltv))
+        + (user_total_collateral.scaled_mul(user_max_ltv))
+        - (amount_taken.scaled_mul(reserve_ltv)))
+    .scaled_div(amount + user_total_collateral - amount_taken);
     result
 }
 // ------------ Real time asset data using XRC ------------
-
-
 
 #[ic_cdk_macros::update]
 pub async fn get_exchange_rates(
@@ -102,42 +104,39 @@ pub async fn get_exchange_rates(
         },
     };
     let res: Result<(GetExchangeRateResult,), (ic_cdk::api::call::RejectionCode, String)> =
-            ic_cdk::api::call::call_with_payment128(
-                // i am changing this id.
-                Principal::from_text("by6od-j4aaa-aaaaa-qaadq-cai").unwrap(),
-                // Principal::from_text("uf6dk-hyaaa-aaaaq-qaaaq-cai").unwrap(),
-                "get_exchange_rate",
-                (args,),
-                1_000_000_000,
-            )
-            .await;
-    
-        match res {
-            Ok(res_value) => match res_value.0 {
-                GetExchangeRateResult::Ok(v) => {
-                    let quote = v.rate;
-                    let pow = 10usize.pow(v.metadata.decimals);
-                    let exchange_rate = quote  / pow as u64 ;
-    
-                    // Multiplying the exchange rate by the amount
-                    let total_value = (quote as u128 * amount) / 1000000000  ;
-    
-                    // Getting the current time
-                    let time = ic_cdk::api::time();
-    
-                    // Return the total value and the time
-                    Ok((total_value, time))
-                }
-                GetExchangeRateResult::Err(e) => Err(format!("ERROR :: {:?}", e)),
-            },
-            Err(error) => Err(format!(
-                "Could not get USD/{} Rate - {:?} - {}",
-                base_asset_symbol.clone(),
-                error.0,
-                error.1
-            )),
-        }
+        ic_cdk::api::call::call_with_payment128(
+            // i am changing this id.
+            Principal::from_text("by6od-j4aaa-aaaaa-qaadq-cai").unwrap(),
+            // Principal::from_text("uf6dk-hyaaa-aaaaq-qaaaq-cai").unwrap(),
+            "get_exchange_rate",
+            (args,),
+            1_000_000_000,
+        )
+        .await;
+
+    match res {
+        Ok(res_value) => match res_value.0 {
+            GetExchangeRateResult::Ok(v) => {
+                let quote = v.rate;
+                let pow = 10usize.pow(v.metadata.decimals);
+                let exchange_rate = quote / pow as u64;
+
+                // Multiplying the exchange rate by the amount
+                let total_value = (quote as u128 * amount) / 1000000000;
+
+                // Getting the current time
+                let time = ic_cdk::api::time();
+
+                // Return the total value and the time
+                Ok((total_value, time))
+            }
+            GetExchangeRateResult::Err(e) => Err(format!("ERROR :: {:?}", e)),
+        },
+        Err(error) => Err(format!(
+            "Could not get USD/{} Rate - {:?} - {}",
+            base_asset_symbol.clone(),
+            error.0,
+            error.1
+        )),
     }
-    
-
-
+}

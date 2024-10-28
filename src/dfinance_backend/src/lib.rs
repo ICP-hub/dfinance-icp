10784a1aa075c7d24ae69522b70eb2fda81ead78
anshika1307-code <anshika.goel@quadbtech.com>
2024-10-28 17:44:14 +0530

    new login function

--- src/dfinance_backend/src/lib.rs
+++ src/dfinance_backend/src/lib.rs
@@ -13,6 +13,8 @@ use implementations::reserve;
 use protocol::libraries::math::calculate::calculate_health_factor;
 use protocol::libraries::math::calculate::calculate_ltv;
 use protocol::libraries::math::calculate::UserPosition;
+use protocol::libraries::math::math_utils;
+use protocol::libraries::math::math_utils::ScalingMath;
 use protocol::libraries::types::datatypes::UserReserveData;
 mod api;
 mod constants;
@@ -242,11 +244,73 @@ pub async fn withdraw(
     }
 }
 
+
+
+
+fn update_user_reserve_state(user_reserve_data: &mut UserReserveData) -> Result<(), String> {
+    let current_time = ic_cdk::api::time() / 1_000_000_000;
+    ic_cdk::println!("Current timestamp: {}", current_time);
+
+    if user_reserve_data.last_update_timestamp == current_time {
+        ic_cdk::println!("No update needed as timestamps match.");
+        return Ok(());
+    }
+
+    // Calculate liquidity index update based on supply rate
+    if user_reserve_data.supply_rate != 0 {
+        let cumulated_liquidity_interest = math_utils::calculate_linear_interest(
+            user_reserve_data.supply_rate,
+            user_reserve_data.last_update_timestamp,
+        );
+        ic_cdk::println!(
+            "Calculated cumulated liquidity interest: {} based on supply rate: {}",
+            cumulated_liquidity_interest,
+            user_reserve_data.supply_rate
+        );
+
+        user_reserve_data.liquidity_index = cumulated_liquidity_interest.scaled_mul(user_reserve_data.liquidity_index);
+        ic_cdk::println!(
+            "Updated liquidity index: {} for reserve",
+            user_reserve_data.liquidity_index
+        );
+    }
+
+    // Calculate debt index update based on borrow rate
+    if user_reserve_data.variable_borrow_index != 0 {
+        let cumulated_borrow_interest = math_utils::calculate_compounded_interest(
+            user_reserve_data.borrow_rate,
+            user_reserve_data.last_update_timestamp,
+            current_time,
+        );
+        ic_cdk::println!(
+            "Calculated cumulated borrow interest: {} based on borrow rate: {}",
+            cumulated_borrow_interest,
+            user_reserve_data.borrow_rate
+        );
+
+        user_reserve_data.variable_borrow_index = cumulated_borrow_interest.scaled_mul(user_reserve_data.variable_borrow_index);
+        ic_cdk::println!(
+            "Updated variable borrow index: {} for reserve",
+            user_reserve_data.variable_borrow_index
+        );
+    }
+
+    // Update last update timestamp
+    user_reserve_data.last_update_timestamp = current_time;
+    ic_cdk::println!(
+        "Updated last update timestamp for reserve: {}",
+        user_reserve_data.last_update_timestamp
+    );
+
+    Ok(())
+}
+
 #[update]
 pub fn login() -> Result<(), String> {
     let user_principal = ic_cdk::caller();
+    ic_cdk::println!("User principal: {}", user_principal);
 
-    // fetch user data.
+    // Fetch user data
     let user_data_result = mutate_state(|state| {
         state
             .user_profile
@@ -259,6 +323,7 @@ pub fn login() -> Result<(), String> {
         Ok(data) => data,
         Err(err) => return Err(err),
     };
+
     // Ensure reserves exist for the user
     let reserves = user_data
         .reserves
@@ -266,33 +331,44 @@ pub fn login() -> Result<(), String> {
         .ok_or_else(|| format!("Reserves not found for user {}", user_principal.to_string()))?;
 
     let current_timestamp = ic_cdk::api::time() / 1_000_000_000;
+    ic_cdk::println!("Current timestamp in login: {}", current_timestamp);
 
     let mut total_collateral = user_data.total_collateral.unwrap_or(0);
+    ic_cdk::println!("Initial total collateral: {}", total_collateral);
 
-    for (reserve_name, reserve_data) in reserves.iter_mut() {
+    for (reserve_name, user_reserve_data) in reserves.iter_mut() {
         ic_cdk::println!("Processing reserve: {}", reserve_name);
 
-        // Update the liquidity index based on APY and time difference
-        let delta_time = current_timestamp - reserve_data.last_update_timestamp;
-        let apy = reserve_data.supply_rate; // Assuming supply_rate represents APY in basis points
-        ic_cdk::println!("to check the apy rate = {}", apy);
-        ic_cdk::println!("to check the time = {}", delta_time);
-        ic_cdk::println!("to check the current time stamp = {}", current_timestamp);
+        update_user_reserve_state(user_reserve_data)?;
 
-        update_liquidity_index(reserve_data, apy, delta_time)?;
-
-        // Dynamically calculate the user's updated balance using the liquidity index
         let updated_balance = calculate_dynamic_balance(
-            reserve_data.asset_supply,
-            reserve_data.liquidity_index,
-            reserve_data.variable_borrow_index, // Assuming this is the initial liquidity index
+            user_reserve_data.asset_supply,
+            user_reserve_data.liquidity_index,
+            user_reserve_data.variable_borrow_index,
+        );
+        ic_cdk::println!(
+            "Updated balance calculated using liquidity index {}: {}",
+            user_reserve_data.liquidity_index,
+            updated_balance
         );
 
-        reserve_data.asset_supply = updated_balance;
+        user_reserve_data.asset_supply = updated_balance;
+        ic_cdk::println!(
+            "Updated asset supply for reserve {}: {}",
+            reserve_name,
+            user_reserve_data.asset_supply
+        );
 
-        // Update the user's total collateral based on the updated reserve balance
-        if reserve_data.asset_supply > 0 {
-            total_collateral += updated_balance - reserve_data.asset_supply;
+    
+        if user_reserve_data.asset_supply > 0 && user_reserve_data.is_collateral {
+            let added_collateral = updated_balance - user_reserve_data.asset_supply;
+            total_collateral += added_collateral;
+            ic_cdk::println!(
+                "Added to total collateral from reserve {}: {} (New total: {})",
+                reserve_name,
+                added_collateral,
+                total_collateral
+            );
         }
 
         let user_position = UserPosition {
@@ -301,42 +377,135 @@ pub fn login() -> Result<(), String> {
             liquidation_threshold: user_data.liquidation_threshold.unwrap(),
         };
 
-        if reserve_data.asset_supply > 0 || reserve_data.asset_borrow > 0 {
+  
+        if user_reserve_data.asset_supply > 0 || user_reserve_data.asset_borrow > 0 {
             user_data.health_factor = Some(calculate_health_factor(&user_position));
             user_data.ltv = Some(calculate_ltv(&user_position));
+            ic_cdk::println!(
+                "Updated health factor: {}, Updated LTV: {}",
+                user_data.health_factor.unwrap(),
+                user_data.ltv.unwrap()
+            );
         }
 
-        // Update the timestamp for this reserve
-        reserve_data.last_update_timestamp = current_timestamp;
+        user_reserve_data.last_update_timestamp = current_timestamp;
+        ic_cdk::println!(
+            "Updated last update timestamp for reserve {}: {}",
+            reserve_name,
+            user_reserve_data.last_update_timestamp
+        );
     }
 
-    // Update the user's total collateral value
-    user_data.total_collateral = Some(total_collateral);
 
-    // Update the user profile with the new state
+    user_data.total_collateral = Some(total_collateral);
+    ic_cdk::println!("Final total collateral: {}", total_collateral);
+
     mutate_state(|state| {
         state
             .user_profile
             .insert(user_principal, declarations::storable::Candid(user_data));
     });
+    ic_cdk::println!("User profile updated successfully for user: {}", user_principal);
 
     Ok(())
 }
 
-fn update_liquidity_index(
-    reserve_data: &mut UserReserveData,
-    liquidity_rate: u128, // APY of the reserve in basis points (e.g., 5% = 500)
-    delta_time: u64,      // Time difference in seconds
-) -> Result<(), String> {
-    // Convert the liquidity rate (APY) to per second rate
-    let liquidity_rate_per_second = liquidity_rate as u128 * 1_000_000_000 / (365 * 24 * 60 * 60);
 
-    // Update the liquidity index with the new value based on delta_time
-    reserve_data.liquidity_index =
-        reserve_data.liquidity_index * (1 + liquidity_rate_per_second * delta_time as u128);
+// #[update]
+// pub fn login() -> Result<(), String> {
+//     let user_principal = ic_cdk::caller();
 
-    Ok(())
-}
+//     // fetch user data.
+//     let user_data_result = mutate_state(|state| {
+//         state
+//             .user_profile
+//             .get(&user_principal)
+//             .map(|user| user.0.clone())
+//             .ok_or_else(|| format!("User not found: {}", user_principal.to_string()))
+//     });
+
+//     let mut user_data = match user_data_result {
+//         Ok(data) => data,
+//         Err(err) => return Err(err),
+//     };
+//     // Ensure reserves exist for the user
+//     let reserves = user_data
+//         .reserves
+//         .as_mut()
+//         .ok_or_else(|| format!("Reserves not found for user {}", user_principal.to_string()))?;
+
+//     let current_timestamp = ic_cdk::api::time() / 1_000_000_000;
+
+//     let mut total_collateral = user_data.total_collateral.unwrap_or(0);
+
+//     for (reserve_name, reserve_data) in reserves.iter_mut() {
+//         ic_cdk::println!("Processing reserve: {}", reserve_name);
+
+//         // Update the liquidity index based on APY and time difference
+//         let delta_time = current_timestamp - reserve_data.last_update_timestamp;
+//         let apy = reserve_data.supply_rate; // Assuming supply_rate represents APY in basis points
+//         ic_cdk::println!("to check the apy rate = {}", apy);
+//         ic_cdk::println!("to check the time = {}", delta_time);
+//         ic_cdk::println!("to check the current time stamp = {}", current_timestamp);
+
+//         update_liquidity_index(reserve_data, apy, delta_time)?;
+
+//         // Dynamically calculate the user's updated balance using the liquidity index
+//         let updated_balance = calculate_dynamic_balance(
+//             reserve_data.asset_supply,
+//             reserve_data.liquidity_index,
+//             reserve_data.variable_borrow_index, // Assuming this is the initial liquidity index
+//         );
+
+//         reserve_data.asset_supply = updated_balance;
+
+//         // Update the user's total collateral based on the updated reserve balance
+//         if reserve_data.asset_supply > 0 {
+//             total_collateral += updated_balance - reserve_data.asset_supply;
+//         }
+
+//         let user_position = UserPosition {
+//             total_collateral_value: total_collateral,
+//             total_borrowed_value: user_data.total_debt.unwrap(),
+//             liquidation_threshold: user_data.liquidation_threshold.unwrap(),
+//         };
+
+//         if reserve_data.asset_supply > 0 || reserve_data.asset_borrow > 0 {
+//             user_data.health_factor = Some(calculate_health_factor(&user_position));
+//             user_data.ltv = Some(calculate_ltv(&user_position));
+//         }
+
+//         // Update the timestamp for this reserve
+//         reserve_data.last_update_timestamp = current_timestamp;
+//     }
+
+//     // Update the user's total collateral value
+//     user_data.total_collateral = Some(total_collateral);
+
+//     // Update the user profile with the new state
+//     mutate_state(|state| {
+//         state
+//             .user_profile
+//             .insert(user_principal, declarations::storable::Candid(user_data));
+//     });
+
+//     Ok(())
+// }
+
+// fn update_liquidity_index(
+//     reserve_data: &mut UserReserveData,
+//     liquidity_rate: u128, // APY of the reserve in basis points (e.g., 5% = 500)
+//     delta_time: u64,      // Time difference in seconds
+// ) -> Result<(), String> {
+//     // Convert the liquidity rate (APY) to per second rate
+//     let liquidity_rate_per_second = liquidity_rate as u128 * 1_000_000_000 / (365 * 24 * 60 * 60);
+
+//     // Update the liquidity index with the new value based on delta_time
+//     reserve_data.liquidity_index =
+//         reserve_data.liquidity_index * (1 + liquidity_rate_per_second * delta_time as u128);
+
+//     Ok(())
+// }
 
 fn calculate_dynamic_balance(
     initial_deposit: u128,         // The initial deposit amount (asset_supply)
@@ -347,7 +516,7 @@ fn calculate_dynamic_balance(
     initial_deposit * current_liquidity_index / deposit_liquidity_index
 }
 
-//approve function that take input  - amount and asset name -> e.g "ckBTC " -> retrive its principal from reserve
+// //approve function that take input  - amount and asset name -> e.g "ckBTC " -> retrive its principal from reserve
 //call approve transfer function -- function.rs
 // backend canister as spender
 //caller as from

eddde16bace2eb9398906a1fad7ea612a36134c6
anshika1307-code <anshika.goel@quadbtech.com>
2024-11-12 16:29:08 +0530

    corrected calculate_compounded_interest function

--- src/dfinance_backend/src/protocol/libraries/math/math_utils.rs
+++ src/dfinance_backend/src/protocol/libraries/math/math_utils.rs
@@ -2,13 +2,14 @@ use std::ops::{Div, Mul};
 
 const SCALING_FACTOR: u128 = 100_000_000; //10^8
 const SECONDS_PER_YEAR: u64 = 365 * 24 * 60 * 60; // 365 days
-//leap year calculation
+//leap year calculation left 
 pub fn calculate_linear_interest(rate: u128, last_update_timestamp: u64) -> u128 {
     let current_timestamp = ic_cdk::api::time() / 1_000_000_000; // Convert nanoseconds to seconds
     let time_delta = current_timestamp - last_update_timestamp;
     let result = rate * time_delta as u128 / SECONDS_PER_YEAR as u128;
     SCALING_FACTOR + result
 }
+
 
 pub fn calculate_compounded_interest(
     rate: u128,
@@ -20,18 +21,23 @@ pub fn calculate_compounded_interest(
         return SCALING_FACTOR;
     }
 
+    // Scale rate for the shorter time period (fractional year)
+    let scaled_rate = rate * exp as u128 / SECONDS_PER_YEAR as u128;
+
+    // Calculate interest with the adjusted rate over a small period
     let exp_minus_one = exp - 1;
     let exp_minus_two = if exp > 2 { exp - 2 } else { 0 };
 
-    let base_power_two = rate.mul(rate) / (SECONDS_PER_YEAR as u128).mul(SECONDS_PER_YEAR as u128);
-    let base_power_three = base_power_two.mul(rate) / SECONDS_PER_YEAR as u128;
+    let base_power_two = scaled_rate * scaled_rate / SCALING_FACTOR;
+    let base_power_three = base_power_two * scaled_rate / SCALING_FACTOR;
 
     let second_term = exp as u128 * exp_minus_one as u128 * base_power_two / 2;
     let third_term =
         exp as u128 * exp_minus_one as u128 * exp_minus_two as u128 * base_power_three / 6;
 
-    SCALING_FACTOR + (rate * exp as u128) / SECONDS_PER_YEAR as u128 + second_term + third_term
+    SCALING_FACTOR + scaled_rate + second_term + third_term
 }
+
 
 pub fn calculate_compounded_interest_with_current_timestamp(
     rate: u128,

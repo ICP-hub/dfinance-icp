96fbb8f3192b20c7ce516434145a227b3b75262b
jyotirmaygupta23 <jyotirmay.gupta@quadbtech.com>
2024-11-19 13:21:03 +0530

    updated reserve.rs with two new functions

--- src/dfinance_backend/src/protocol/libraries/logic/reserve.rs
+++ src/dfinance_backend/src/protocol/libraries/logic/reserve.rs
@@ -1,25 +1,24 @@
 use crate::protocol::libraries::math::math_utils;
+use crate::protocol::libraries::types::datatypes::UserReserveData;
 use ic_cdk::api::time;
+use ic_cdk::query;
 
 use crate::declarations::assets::ReserveCache;
 use crate::declarations::assets::ReserveData;
 
-use crate::protocol::libraries::logic::interest_rate::{calculate_interest_rates, initialize_interest_rate_params};
+use crate::protocol::libraries::logic::interest_rate::{
+    calculate_interest_rates, initialize_interest_rate_params,
+};
 use crate::protocol::libraries::math::math_utils::ScalingMath;
 
-
 use crate::protocol::libraries::types::datatypes::UserState;
-
 
 use crate::api::functions::asset_transfer;
 use candid::{Nat, Principal};
 
-
 fn current_timestamp() -> u64 {
     time() / 1_000_000_000 // time() returns nanoseconds.
 }
-    
-
 
 pub fn cache(reserve_data: &ReserveData) -> ReserveCache {
     ReserveCache {
@@ -32,14 +31,16 @@ pub fn cache(reserve_data: &ReserveData) -> ReserveCache {
         next_liquidity_index: reserve_data.liquidity_index,
         curr_liquidity_rate: reserve_data.current_liquidity_rate,
 
-         curr_debt_index: reserve_data.debt_index,
-         curr_debt_rate: reserve_data.borrow_rate,
-         next_debt_rate: reserve_data.borrow_rate,
-         next_debt_index: reserve_data.debt_index,
-         debt_last_update_timestamp: 0,
+        curr_debt_index: reserve_data.debt_index,
+        curr_debt_rate: reserve_data.borrow_rate,
+        next_debt_rate: reserve_data.borrow_rate,
+        next_debt_index: reserve_data.debt_index,
+        debt_last_update_timestamp: 0,
 
-         reserve_factor: reserve_data.configuration.reserve_factor.clone(),
+        reserve_factor: reserve_data.configuration.reserve_factor.clone(),
 
+        curr_debt: reserve_data.asset_borrow,
+        curr_supply: reserve_data.asset_supply,
     }
 }
 
@@ -52,6 +53,7 @@ pub fn update_state(reserve_data: &mut ReserveData, reserve_cache: &mut ReserveC
     }
 
     update_indexes(reserve_data, reserve_cache);
+    accrue_to_treasury(reserve_data, reserve_cache);
 
     reserve_data.last_update_timestamp = current_time;
 }
@@ -62,9 +64,9 @@ pub fn update_indexes(reserve_data: &mut ReserveData, reserve_cache: &mut Reserv
             reserve_cache.curr_liquidity_rate,
             reserve_cache.reserve_last_update_timestamp,
         );
-        reserve_cache.next_liquidity_index = 
+        reserve_cache.next_liquidity_index =
             cumulated_liquidity_interest.scaled_mul(reserve_cache.curr_liquidity_index);
-        
+
         reserve_data.liquidity_index = reserve_cache.next_liquidity_index;
     }
 
@@ -74,45 +76,45 @@ pub fn update_indexes(reserve_data: &mut ReserveData, reserve_cache: &mut Reserv
             reserve_cache.reserve_last_update_timestamp,
             current_timestamp(),
         );
-        reserve_cache.next_debt_index = cumulated_borrow_interest.scaled_mul(reserve_cache.curr_debt_index);
+        reserve_cache.next_debt_index =
+            cumulated_borrow_interest.scaled_mul(reserve_cache.curr_debt_index);
         reserve_data.debt_index = reserve_cache.next_debt_index;
     }
 }
 
-
-
 pub async fn update_interest_rates(
-        reserve_data: &mut ReserveData,
-        reserve_cache: &mut ReserveCache,
-        total_borrowed: u128,
-        total_supplies: u128,
-    ){
-        let total_debt = total_borrowed.scaled_mul(reserve_cache.curr_debt_index);
-        let total_supply= total_supplies.scaled_mul(reserve_cache.curr_liquidity_index);
-        let asset=reserve_data.asset_name.clone().unwrap_or("no token".to_string());
-        let interest_rate_params = initialize_interest_rate_params(&asset);
-        ic_cdk::println!("interest rate params {:?}", interest_rate_params);
-        ic_cdk::println!("total debt: {:?}", total_debt);
-        let (next_liquidity_rate, next_debt_rate) =
-        calculate_interest_rates(
-            total_supply,                        
-            total_borrowed,                    
-            total_debt,                
-            reserve_cache.curr_debt_rate,   
-            &interest_rate_params, 
-            reserve_cache.reserve_factor,           
-           
-        );
-        reserve_data.total_borrowed = total_borrowed;
-        reserve_data.total_supply= total_supply;
-        reserve_data.current_liquidity_rate = next_liquidity_rate;
-        reserve_data.borrow_rate = next_debt_rate;
-        ic_cdk::println!("reserve_data.total_borrowed: {:?}", reserve_data.total_borrowed);
+    reserve_data: &mut ReserveData,
+    reserve_cache: &mut ReserveCache,
+    total_borrowed: u128,
+    total_supplies: u128,
+) {
+    let total_debt = total_borrowed.scaled_mul(reserve_cache.curr_debt_index);
+    let total_supply = total_supplies.scaled_mul(reserve_cache.curr_liquidity_index);
+    let asset = reserve_data
+        .asset_name
+        .clone()
+        .unwrap_or("no token".to_string());
+    let interest_rate_params = initialize_interest_rate_params(&asset);
+    ic_cdk::println!("interest rate params {:?}", interest_rate_params);
+    ic_cdk::println!("total debt: {:?}", total_debt);
+    let (next_liquidity_rate, next_debt_rate) = calculate_interest_rates(
+        total_supply,
+        total_borrowed,
+        total_debt,
+        reserve_cache.curr_debt_rate,
+        &interest_rate_params,
+        reserve_cache.reserve_factor,
+    );
+    reserve_data.asset_borrow = total_borrowed;
+    reserve_data.asset_supply = total_supply;
+    reserve_data.current_liquidity_rate = next_liquidity_rate;
+    reserve_data.borrow_rate = next_debt_rate;
+    ic_cdk::println!(
+        "reserve_data.total_borrowed: {:?}",
+        reserve_data.total_borrowed
+    );
+}
 
-    }
-
-
-  
 pub async fn burn_scaled(
     user_state: &mut UserState,
     amount: u128,
@@ -236,72 +238,116 @@ pub async fn mint_scaled(
     }
 }
 
+pub fn accrue_to_treasury(reserve_data: &mut ReserveData, reserve_cache: &ReserveCache) {
+    let mut vars = AccrueToTreasuryLocalVars::default();
 
-// // pub fn accrue_to_treasury(reserve_data: &mut ReserveData, reserve_cache: &ReserveCache) {
-// //     let mut vars = AccrueToTreasuryLocalVars::default();
+    if reserve_cache.reserve_factor == 0 {
+        return;
+    }
 
-// //     if reserve_cache.reserve_factor == 0 {
-// //         return;
-// //     }
+    vars.prev_total_variable_debt =
+        ScalingMath::scaled_mul(reserve_cache.curr_debt, reserve_cache.curr_debt_index);
 
-// //     // vars.prev_total_variable_debt = ray_mul(
-// //     //     reserve_cache.curr_scaled_variable_debt,
-// //     //     reserve_cache.curr_variable_borrow_index,
-// //     // );
+    vars.curr_total_variable_debt =
+        ScalingMath::scaled_mul(reserve_cache.curr_debt, reserve_cache.next_debt_index);
 
-// //     // vars.curr_total_variable_debt = ray_mul(
-// //     //     reserve_cache.curr_scaled_variable_debt,
-// //     //     reserve_cache.next_variable_borrow_index,
-// //     // );
+    vars.total_debt_accrued = vars.curr_total_variable_debt - vars.prev_total_variable_debt;
 
-// //     vars.cumulated_stable_interest = math_utils::calculate_compounded_interest(
-// //         reserve_cache.curr_avg_stable_borrow_rate,
-// //         reserve_cache.stable_debt_last_update_timestamp,
-// //         reserve_cache.reserve_last_update_timestamp,
-// //     );
+    vars.amount_to_mint =
+        ScalingMath::scaled_mul(vars.total_debt_accrued, reserve_cache.reserve_factor); //percent
 
-// //     // vars.prev_total_stable_debt = ray_mul(
-// //     //     reserve_cache.curr_principal_stable_debt,
-// //     //     vars.cumulated_stable_interest,
-// //     // );
+    if vars.amount_to_mint != 0 {
+        reserve_data.accure_to_platform +=
+            ScalingMath::scaled_mul(vars.amount_to_mint, reserve_cache.next_liquidity_index)
+                as u128;
+    }
+}
 
-// //     vars.total_debt_accrued = vars.curr_total_variable_debt + reserve_cache.curr_total_stable_debt
-// //         - vars.prev_total_variable_debt
-// //         - vars.prev_total_stable_debt;
+struct AccrueToTreasuryLocalVars {
+    prev_total_variable_debt: u128,
+    curr_total_variable_debt: u128,
+    total_debt_accrued: u128,
+    amount_to_mint: u128,
+}
 
-// //     vars.amount_to_mint = percent_mul(vars.total_debt_accrued, reserve_cache.reserve_factor);
+impl Default for AccrueToTreasuryLocalVars {
+    fn default() -> Self {
+        AccrueToTreasuryLocalVars {
+            prev_total_variable_debt: 0,
+            curr_total_variable_debt: 0,
+            total_debt_accrued: 0,
+            amount_to_mint: 0,
+        }
+    }
+}
 
-// //     // if vars.amount_to_mint != 0 {
-// //     //     reserve_data.accrued_to_treasury +=
-// //     //         ray_div(vars.amount_to_mint, reserve_cache.next_liquidity_index) as u128;
-// //     // }
-// // }
+#[query]
+pub fn user_normalized_supply(user_reserve_data: UserReserveData) -> Result<u128, String> {
+    let current_time = ic_cdk::api::time() / 1_000_000_000;
+    ic_cdk::println!("Current timestamp: {}", current_time);
 
+    if user_reserve_data.last_update_timestamp == current_time {
+        ic_cdk::println!("No update needed as timestamps match.");
+        return Err("No update needed as timestamps match.".to_string());
+    }
 
+    if user_reserve_data.supply_rate != 0 {
+        let cumulated_liquidity_interest = math_utils::calculate_linear_interest(
+            user_reserve_data.supply_rate,
+            user_reserve_data.last_update_timestamp,
+        );
+        ic_cdk::println!(
+            "Calculated cumulated liquidity interest: {} based on supply rate: {}",
+            cumulated_liquidity_interest,
+            user_reserve_data.supply_rate
+        );
 
+        ic_cdk::println!(
+            "Updated liquidity index: {} for reserve",
+            user_reserve_data.liquidity_index
+        );
+        //  user_reserve_data.liquidity_index =
+        return Ok(cumulated_liquidity_interest.scaled_mul(user_reserve_data.liquidity_index));
+    }
+    Ok(user_reserve_data.liquidity_index)
+}
 
+#[query]
+pub fn user_normalized_debt(user_reserve_data: UserReserveData) -> Result<u128, String> {
+    let current_time = ic_cdk::api::time() / 1_000_000_000;
+    ic_cdk::println!("Current timestamp: {}", current_time);
 
-// struct AccrueToTreasuryLocalVars {
-//     prev_total_variable_debt: u128,
-//     curr_total_variable_debt: u128,
-//     cumulated_stable_interest: u128,
-//     prev_total_stable_debt: u128,
-//     total_debt_accrued: u128,
-//     amount_to_mint: u128,
-// }
+    if user_reserve_data.last_update_timestamp == current_time {
+        ic_cdk::println!("No update needed as timestamps match.");
+        return Err("No update needed as timestamps match.".to_string());
+    }
 
-// impl Default for AccrueToTreasuryLocalVars {
-//     fn default() -> Self {
-//         AccrueToTreasuryLocalVars {
-//             prev_total_variable_debt: 0,
-//             curr_total_variable_debt: 0,
-//             cumulated_stable_interest: 0,
-//             prev_total_stable_debt: 0,
-//             total_debt_accrued: 0,
-//             amount_to_mint: 0,
-//         }
-//     }
-// }
+    if user_reserve_data.variable_borrow_index != 0 {
+        ic_cdk::println!(
+            "Previous borrow index & rate: {:?} {:?}",
+            user_reserve_data.variable_borrow_index,
+            user_reserve_data.borrow_rate
+        );
+        let cumulated_borrow_interest = math_utils::calculate_compounded_interest(
+            (user_reserve_data.borrow_rate / 100) as u128,
+            user_reserve_data.last_update_timestamp,
+            current_time,
+        );
+        ic_cdk::println!(
+            "Calculated cumulated borrow interest: {} based on borrow rate: {}",
+            cumulated_borrow_interest,
+            user_reserve_data.borrow_rate //take it from reserve of asset
+        );
+
+        ic_cdk::println!(
+            "Updated variable borrow index: {} for reserve",
+            user_reserve_data.variable_borrow_index
+        );
+        return Ok(cumulated_borrow_interest.scaled_mul(user_reserve_data.variable_borrow_index));
+    }
+
+    Ok(user_reserve_data.variable_borrow_index)
+}
 
 // #[derive(Default, Debug, CandidType, Deserialize, Serialize)]
 // struct UpdateInterestRatesLocalVars {
@@ -311,3 +357,90 @@ pub async fn mint_scaled(
 //     total_variable_debt: u128,
 // }
 
+// function executeMintToTreasury(
+//     mapping(address => DataTypes.ReserveData) storage reservesData,
+//     address[] calldata assets
+//   ) external {
+//     for (uint256 i = 0; i < assets.length; i++) {
+//       address assetAddress = assets[i];
+
+//       DataTypes.ReserveData storage reserve = reservesData[assetAddress];
+
+//       // this cover both inactive reserves and invalid reserves since the flag will be 0 for both
+//       if (!reserve.configuration.getActive()) {
+//         continue;
+//       }
+
+//       uint256 accruedToTreasury = reserve.accruedToTreasury;
+
+//       if (accruedToTreasury != 0) {
+//         reserve.accruedToTreasury = 0;
+//         uint256 normalizedIncome = reserve.getNormalizedIncome();
+//         uint256 amountToMint = accruedToTreasury.rayMul(normalizedIncome);
+//         IAToken(reserve.aTokenAddress).mintToTreasury(amountToMint, normalizedIncome);
+
+//         emit MintedToTreasury(assetAddress, amountToMint);
+//       }
+//     }
+//   }
+
+// function mintToTreasury(address[] calldata assets) external virtual override {
+//     PoolLogic.executeMintToTreasury(_reserves, assets);
+//   }
+
+///// @inheritdoc IAToken
+// function mintToTreasury(uint256 amount, uint256 index) external virtual override onlyPool {
+//     if (amount == 0) {
+//       return;
+//     }
+//     _mintScaled(address(POOL), _treasury, amount, index);
+//   }
+
+//  /**
+//    * @notice Mints the assets accrued through the reserve factor to the treasury in the form of aTokens
+//    * @param assets The list of reserves for which the minting needs to be executed
+//    */
+//   function mintToTreasury(address[] calldata assets) external;
+//   /**
+//    * @notice Implements the basic logic to mint a scaled balance token.
+//    * @param caller The address performing the mint
+//    * @param onBehalfOf The address of the user that will receive the scaled tokens
+//    * @param amount The amount of tokens getting minted
+//    * @param index The next liquidity index of the reserve
+//    * @return `true` if the the previous balance of the user was 0
+//    */
+//   function _mintScaled(
+//     address caller,
+//     address onBehalfOf,
+//     uint256 amount,
+//     uint256 index
+//   ) internal returns (bool) {
+//     uint256 amountScaled = amount.rayDiv(index);
+//     require(amountScaled != 0, Errors.INVALID_MINT_AMOUNT);
+
+//     uint256 scaledBalance = super.balanceOf(onBehalfOf);
+//     uint256 balanceIncrease = scaledBalance.rayMul(index) -
+//       scaledBalance.rayMul(_userState[onBehalfOf].additionalData);
+
+//     _userState[onBehalfOf].additionalData = index.toUint128();
+
+//     _mint(onBehalfOf, amountScaled.toUint128());
+
+//     uint256 amountToMint = amount + balanceIncrease;
+//     emit Transfer(address(0), onBehalfOf, amountToMint);
+//     emit Mint(caller, onBehalfOf, amountToMint, balanceIncrease, index);
+
+//     return (scaledBalance == 0);
+//   }
+
+// function mint(
+//     address user,
+//     address onBehalfOf,
+//     uint256 amount,
+//     uint256 index
+//   ) external virtual override onlyPool returns (bool, uint256) {
+//     if (user != onBehalfOf) {
+//       _decreaseBorrowAllowance(onBehalfOf, user, amount);
+//     }
+//     return (_mintScaled(user, onBehalfOf, amount, index), scaledTotalSupply());
+//   }

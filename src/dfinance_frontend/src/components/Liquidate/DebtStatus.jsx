8f95a80a5fcbf4477fb77d38cd9190a05fb551e0
Shreya Mahajan <shreya.mahajan@quadbtech.com>
2024-12-19 17:07:03 +0530

    liquidation tour added collateral assets fetched for all the principle and fixe d popup to show assets correctly

--- src/dfinance_frontend/src/components/Liquidate/DebtStatus.jsx
+++ src/dfinance_frontend/src/components/Liquidate/DebtStatus.jsx
@@ -275,7 +275,7 @@ const DebtStatus = () => {
         .filter(
           (mappedItem) =>
             mappedItem &&
-            mappedItem.healthFactor > 1 &&
+            mappedItem.healthFactor < 1 &&
             mappedItem.principal !== principal &&
             mappedItem.totalDebt > 0
         );

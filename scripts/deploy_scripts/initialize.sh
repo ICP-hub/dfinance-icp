1db00231be41237502a7b64a3efc6b80672a7a9d
anshika1307-code <anshika.goel@quadbtech.com>
2024-12-26 16:26:33 +0530

    initialize script for local

--- scripts/deploy_scripts/initialize.sh
+++ scripts/deploy_scripts/initialize.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Set the deployer identity
-echo "Switching to 'deployer' identity..."
-dfx identity use deployer
+# Set the deployer identity 
+# echo "Switching to 'deployer' identity..."
+# dfx identity use deployer
+
+# for local
+echo "Switching to 'default' identity..."
+dfx identity use default
 
 # Call the empty function
 echo "Calling 'initialize_canister' on 'dfinance_backend' canister..."

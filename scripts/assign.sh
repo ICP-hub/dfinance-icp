f0f07ebcfaf62c4b2ab567fff76e4c59507caac8
anshika-Quadbcode <anshika.goel@quadbtech.com>
2025-02-11 12:46:12 +0530

    deploy changes

--- scripts/assign.sh
+++ scripts/assign.sh
@@ -1,5 +1,5 @@
-dfx canister update-settings debttoken --add-controller sbsfl-7capx-5vx4r-thc6m-si77l-mmfdc-gkrh3-6e4uk-ifxh7-gnijx-uae --ic
-dfx canister update-settings dfinance_backend --add-controller sbsfl-7capx-5vx4r-thc6m-si77l-mmfdc-gkrh3-6e4uk-ifxh7-gnijx-uae --ic
-dfx canister update-settings dfinance_frontend --add-controller sbsfl-7capx-5vx4r-thc6m-si77l-mmfdc-gkrh3-6e4uk-ifxh7-gnijx-uae --ic
-dfx canister update-settings dtoken --add-controller sbsfl-7capx-5vx4r-thc6m-si77l-mmfdc-gkrh3-6e4uk-ifxh7-gnijx-uae --ic
-dfx canister update-settings token_ledger --add-controller sbsfl-7capx-5vx4r-thc6m-si77l-mmfdc-gkrh3-6e4uk-ifxh7-gnijx-uae --ic
+dfx canister update-settings debttoken --add-controller 3xaix-z6m7f-5etpz-h4mar-oy5ly-e7qv7-na332-6rsqg-gss46-cy5sz-jae --ic
+dfx canister update-settings dfinance_backend --add-controller 3xaix-z6m7f-5etpz-h4mar-oy5ly-e7qv7-na332-6rsqg-gss46-cy5sz-jae --ic
+dfx canister update-settings dfinance_frontend --add-controller 3xaix-z6m7f-5etpz-h4mar-oy5ly-e7qv7-na332-6rsqg-gss46-cy5sz-jae --ic
+dfx canister update-settings dtoken --add-controller 3xaix-z6m7f-5etpz-h4mar-oy5ly-e7qv7-na332-6rsqg-gss46-cy5sz-jae --ic
+dfx canister update-settings token_ledger --add-controller 3xaix-z6m7f-5etpz-h4mar-oy5ly-e7qv7-na332-6rsqg-gss46-cy5sz-jae --ic
